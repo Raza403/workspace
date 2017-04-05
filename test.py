aa4452423b8dd2f82cc688eccbd1192a69b04526
raza403 <raza403@gmail.com>
2017-04-05 05:01:43 +0000

    after working on buy page bug

--- test.py
+++ test.py
@@ -7,12 +7,12 @@ def sell():
         sym = request.form.get("symbol")
         if not sym:
             return apology ("Must enter the symbol")
-        ss = request.form.get("number")
-        numb = float (ss)
+        num = request.form.get("number")
+        numb = float (num)
         if numb is None or numb == '' or numb < 1:
             return apology ("Please enter valid number of stocks to sell")
         ide = session["user_id"]
-        ro = db.execute ("select * from users where id = :ii" , ii = ide)
+        row = db.execute ("select * from users where id = :ii" , ii = ide)
         #take info about sahres user has
         sybl = db.execute ("select * from portfolio where id = :ii GROUP BY symbol ORDER BY symbol" , ii = ide)
         #if user has not bought any sahres
@@ -27,7 +27,7 @@ def sell():
         else:
             qtd = quoted["price"]
             prc = float(qtd) * numb
-            db.execute("UPDATE users SET cash = :cash WHERE id = :ide",cash = ro[0]["cash"] + prc, ide = ide)
+            db.execute("UPDATE users SET cash = :cash WHERE id = :ide",cash = row[0]["cash"] + prc, ide = ide)
             db.execute("UPDATE portfolio SET shares = :shares WHERE id = :ide AND symbol = :smb",shares = sybl[0]["shares"] - numb,ide = ide,smb = sym)
             return redirect(url_for("index"))
     else:
